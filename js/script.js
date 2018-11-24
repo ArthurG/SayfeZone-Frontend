370cbc181903d80d1cb5df21bca627cd63852c58
ArthurG <arthur.guo@uwaterloo.ca>
2018-11-24 13:20:18 -0500

    Populate graph and video row with actual data

--- js/script.js
+++ js/script.js
@@ -30,18 +30,66 @@ let fakeData =
     {
       id:123,
       timestamp:1543064163,
-      sentiment:-0.5,
+      sentiment:0.5,
       magnitutde:0.5,
       result: [ {    "text": {     "content": "This guy is a stupid ass butt."    },    "sentiment": {     "magnitude": 0.5,     "score": -0.5    }   },   {    "text": {     "content": "I wish he would die already."    },    "sentiment": {     "magnitude": 0.1,     "score": 0.1    }   },   {    "text": {     "content": "You're so dumb you dork."    },    "sentiment": {     "magnitude": 0.7,     "score": -0.7    }   },   {    "text": {     "content": "What is wrong with you?"    },    "sentiment": {     "magnitude": 0.6,     "score": -0.6    }   },   {    "text": {     "content": "Idiot."    },    "sentiment": {     "magnitude": 0.8,     "score": -0.8    }   }  ],
       video_link:"https://firebasestorage.googleapis.com/v0/b/sayfezone.appspot.com/o/WIN_20181124_04_16_43_Pro.mp4?alt=media&token=9fc66ef0-9742-4e10-aa47-55d5e0fc17f8"
     }
   ]
 
+// Funcition to test newVideo. Do not call
 function copyVideo() {
   newVideo("I really like cake... and pie", 12, "Nov 24, 2018 9:55AM");
 }
 
-function newVideo(text, sentiment, timestamp) {
+// Deletes all the videos in the row
+function deleteAllVideos(){
+  $("#addVideos").children().empty();
+}
+
+// Hits an endpoint to get all the video data. Then, populates the video onto the DOM. 
+// Also updates the graph to include the videos
+function getAllVideos(){
+  for (var video of fakeData){
+    let text = "";
+    for (var resultObj of video.result){
+      text+= resultObj.text.content;
+      text+=" "
+    }
+    newVideo(text, video.sentiment, video.timestamp, video.video_link);
+  }
+
+  let data1 = ["sentiment"]
+  let x_labels = []
+  for (var video of fakeData){
+    data1.push(video.sentiment);
+    let d = new Date(video.timestamp);
+    x_labels.push(d.toUTCString());
+  }
+
+
+  var chart = c3.generate({
+    bindto: '#chart',
+    data: {
+      columns: [
+        data1
+      ],
+      onclick: function (data) { 
+        console.log(data.value);
+        console.log(data.name);
+      },
+    },
+    axis: {
+      x: {
+        type: 'category',
+        x_labels
+      }
+    },
+  });
+}
+
+// Adds video to dom
+function newVideo(text, sentiment, timestamp, link) {
   let oldVideo = $("#sample-video");
   let newVideo = oldVideo.clone();
   newVideo.attr("id", "")
@@ -53,26 +101,7 @@ function newVideo(text, sentiment, timestamp) {
     newVideo.find(".sentiment-neg").text(sentiment)
     newVideo.find(".sentiment-pos").hide()
   }
-  newVideo.find(".timestamp").text("Nov 24, 2018 9:55AM")
-  newVideo.find(".speech2text").text("I really like cake.. And pie")
+  let d = new Date(timestamp);
+  newVideo.find(".timestamp").text(d.toUTCString())
+  newVideo.find(".speech2text").text(text)
 }
-
-var chart = c3.generate({
-  bindto: '#chart',
-  data: {
-    columns: [
-      ['data1', 30, 200, 100, 400, 150, 250],
-      ['data2', 50, 20, 10, 40, 15, 25]
-    ],
-    onclick: function (data) { 
-      console.log(data.value);
-      console.log(data.name);
-    },
-  },
-  axis: {
-    x: {
-      type: 'category',
-      categories: ['Nov 1, 2016', 'Nov 2, 2016', 'Nov 4', 'Nov 9 2017', 'Nov 10 2018', 'Nov 12 2019']
-    }
-  },
-});
