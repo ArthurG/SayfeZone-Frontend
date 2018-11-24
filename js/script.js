8953722d8d1530c0e2863215fc7a168eb21e0f3b
ArthurG <arthur.guo@uwaterloo.ca>
2018-11-24 10:47:42 -0500

    Add c3 backend

--- js/script.js
+++ js/script.js
@@ -17,7 +17,7 @@ db.ref('users/eeB7lr84oHfqMntRDTZ0').set({
 });
 */
 
-function copyVideo(){
+function copyVideo() {
   newVideo("I really like cake... and pie", 12, "Nov 24, 2018 9:55AM");
 }
 
@@ -36,3 +36,23 @@ function newVideo(text, sentiment, timestamp) {
   newVideo.find(".timestamp").text("Nov 24, 2018 9:55AM")
   newVideo.find(".speech2text").text("I really like cake.. And pie")
 }
+
+var chart = c3.generate({
+  bindto: '#chart',
+  data: {
+    columns: [
+      ['data1', 30, 200, 100, 400, 150, 250],
+      ['data2', 50, 20, 10, 40, 15, 25]
+    ],
+    onclick: function (data) { 
+      console.log(data.value);
+      console.log(data.name);
+    },
+  },
+  axis: {
+    x: {
+      type: 'category',
+      categories: ['Nov 1, 2016', 'Nov 2, 2016', 'Nov 4', 'Nov 9 2017', 'Nov 10 2018', 'Nov 12 2019']
+    }
+  },
+});
