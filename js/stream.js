2c1fbd2bb595c21f8dd05f74664b75aaaa18d082
Roland Li <roland_li@hotmail.ca>
2018-11-25 03:55:16 -0500

    Actually working choppy stream

--- js/stream.js
+++ js/stream.js
@@ -1,12 +1,11 @@
 let lastTime = Math.round((new Date()).getTime() / 1000) - 1000;
 let selectedVid = "";
 let newVideo;
-let oldVideo;
+let oldVideo = $("#stream");
 let useAlt = true; //True = use old
 
   function startStream(){
     //Set up the references to the doms
-    oldVideo = $("#stream");
     // newVideo = oldVideo.clone();
     // $("#addVideos").prepend(newVideo);
 
@@ -16,13 +15,16 @@ let useAlt = true; //True = use old
   function setupVideo(){
     updateDom();
     useAlt = !useAlt;
-    setTimeout(startStream, 10000);
+    // setTimeout(startStream, 10000);
   }
 
   function updateDom(){
-    // newVideo.attr("id", "sample-video");
-    // oldVideo.attr("id" ,"sample-video");
-    var target = oldVideo;
+    let oldVideo = $("#sample-video");
+    let newVideo = oldVideo.clone();
+    newVideo.attr("id", "");
+    // $("#addVideos").pop();
+    $("#addVideos").empty();
+    $("#addVideos").prepend(newVideo);
     // if (useAlt){
     //   target = oldVideo;
     // }
@@ -35,22 +37,34 @@ let useAlt = true; //True = use old
       sentiment = selectedVid.sentimentData.documentSentiment.score;
     }
 
+    var text = "";
+    if(selectedVid != undefined && selectedVid.sentimentData != undefined && selectedVid.sentimentData.sentences){
+      for (var resultObj of selectedVid.sentimentData.sentences){
+        text+= resultObj.text.content;
+        text+=" "
+      }
+    }
+
 
     //Update whatever dom
     if(sentiment > 0){
-      target.find(".sentiment-pos").text(sentiment);
-      target.find(".sentiment-neg").hide();
+      newVideo.find(".sentiment-pos").text(sentiment);
+      newVideo.find(".sentiment-neg").hide();
     } else {
-      target.find(".sentiment-neg").text(sentiment);
-      target.find(".sentiment-pos").hide();
+      newVideo.find(".sentiment-neg").text(sentiment);
+      newVideo.find(".sentiment-pos").hide();
     }
-    target.find(".timestamp").text(selectedVid.timestamp);
-    target.find(".vid-link").attr("src", selectedVid.video_link);
+    newVideo.find(".timestamp").text(selectedVid.timestamp);
+    newVideo.find(".vid-link").attr("src", selectedVid.video_link);
+    newVideo.find(".speech2text").text(text)
     // target.find(".video").play();
-    let vid = target.find(".video");
-    vid[0].onplay = function(){
-      clearInterval(refreshTimer);
-    }
+    // let vid = newVideo.find(".video");
+    // vid[0].onend = function(){
+    //   clearInterval(startStream);
+    // }
+    $('video').on('ended',function(){
+      startStream();
+    });
   }
     
   // Hits an endpoint to get all the video data. Then, populates the video onto the DOM. 
@@ -60,8 +74,6 @@ let useAlt = true; //True = use old
       url: "https://searchandprotech.lib.id/sayfezonefilter@dev/",
       success: function(item){
         allVideos = item;
-
-        console.log(lastTime);
      
         // selectedVid = "";
         // Find the closest video to now]
@@ -70,10 +82,10 @@ let useAlt = true; //True = use old
             var thisTime = video.timestamp;
             if ( thisTime > lastTime){
                 selectedVid = video;
-                console.log(video);
+                // console.log(video);
                 break;
             }
-            console.log(video.timestamp);
+            // console.log(video.timestamp);
         }
         lastTime = selectedVid.timestamp; 
 
